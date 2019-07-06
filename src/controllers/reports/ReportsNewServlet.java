96d73f6a7cff54ac1640a1c0ce52ba23dd08444a
haruka.sakai <tach.ha2019@gmail.com>
2019-07-06 11:46:22 +0900

    Add Report index

---
+++ src/controllers/reports/ReportsNewServlet.java
@@ -0,0 +1,41 @@
+package controllers.reports;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Report;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class ReportsNewServlet
+ */
+@WebServlet("/reports/new")
+public class ReportsNewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReportsNewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        }
+
+        RequestDispatcher rd =request.getRequestDispatcher("/WEB-INF/views/reports/index.jsp");
+        rd.forward(request, response);
+    }
+}
